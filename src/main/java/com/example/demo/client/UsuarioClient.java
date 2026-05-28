33e832e32b3972b692457be43b9d637d18cd8f94
Dylan Fernandez <dy.fernandezl@duocuc.cl>
2026-05-28 18:16:05 -0400

    Ultimos arreglos para la comunicacion de los microservicios

--- src/main/java/com/example/demo/client/UsuarioClient.java
+++ src/main/java/com/example/demo/client/UsuarioClient.java
@@ -1,10 +1,11 @@
 package com.example.demo.client;
 
+import com.example.demo.dto.UsuarioResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-usuario", url = "http://localhost:8081/api/usuarios")
+@FeignClient(name = "ms-usuario", url = "http://localhost:8081/api/usuario")
 public interface UsuarioClient {
 
     @GetMapping("/{id}")
