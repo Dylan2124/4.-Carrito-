abd02f827703ee44d0772dff3f48c292840563f5
Dylan Fernandez <dy.fernandezl@duocuc.cl>
2026-05-28 18:39:09 -0400

    Ultimos arreglos para la comunicacion de los microservicios

--- src/main/java/com/example/demo/client/CatalogoClient.java
+++ src/main/java/com/example/demo/client/CatalogoClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms_catalogo", url = "http://localhost:8082/api/producto")
+@FeignClient(name = "ms-catalogo", url = "http://localhost:8082/api/producto")
 public interface CatalogoClient {
     @GetMapping("/{id}")
     ProductoResponseDTO obtenerProductoPorId(@PathVariable("id") Long idProducto);
